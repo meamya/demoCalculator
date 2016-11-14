6ff4c19386ef48a3053d1d424fafaa5a6d915165
Meamya <meamyac@yahoo.com>
2016-11-14 12:02:33 -0700

    change

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //this is my main js file
 
-function addNums(){
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var sum = num1 = num2;
-    var totalAmountDisplay = document.getElementById("totalAmountDisplay");
-   var total = document.createTextNode(sum).value;
-   totalAmountDisplay.appendChild(total);
+function addNums() {
+   var num1 = parseInt(document.getElementById("num1").value);
+   var num2 = parseInt(document.getElementById("num2").value);
+   console.log(typeof num1);
+   var sum = num1 + num2;
+   var totalAmountDisplay = document.getElementById("totalAmountDisplay");
+   totalAmountDisplay.innerHTML = sum;
+   // var total = document.createTextNode(sum);
+   // totalAmountDisplay.appendChild(total);
 }
